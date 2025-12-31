9fa2f7516df268a959f5843b9b19e86ebbd4b7ca
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-12-31 16:18:37 +0530

    set correct icon

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=
+export ICON=/usr/share/pixmaps/stm32cubemx.png
 export DESKTOP=https://aur.archlinux.org/cgit/aur.git/plain/stm32cubemx.desktop?h=stm32cubemx
 
 # Deploy dependencies
