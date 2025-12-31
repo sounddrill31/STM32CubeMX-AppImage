3d0eda39064c67c889fd6a4c51604cec88a2586f
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-12-31 16:33:29 +0530

    Add archlinux-java-run to aur deps

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
+make-aur-package archlinux-java-run 
 make-aur-package stm32cubemx
 
 # If the application needs to be manually built that has to be done down here
